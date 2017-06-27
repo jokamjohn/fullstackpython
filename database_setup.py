3f05472c3444f171cd78d42d77b1c5658da68ad8
John Kagga <johnkagga@gmail.com>
2017-06-28 00:31:15 +0300

    creating classes

--- database_setup.py
+++ database_setup.py
@@ -4,4 +4,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 
 Base = declarative_base()
+
+
+class Restaurant(Base):
+    __tablename__ = "restaurant"
+
+class MenuItem(Base):
+    __tablename__ = "menu_item"
+
 engine = create_engine("sqlite:///restaurantmenu.db")
