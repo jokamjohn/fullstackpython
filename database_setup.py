4e1da74362a9674773435bcf9f6ebe2767292fe8
John Kagga <johnkagga@gmail.com>
2017-06-28 00:40:05 +0300

    Mapper - creating table columns

--- database_setup.py
+++ database_setup.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationships
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 
@@ -9,7 +9,21 @@ Base = declarative_base()
 class Restaurant(Base):
     __tablename__ = "restaurant"
 
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+
+
 class MenuItem(Base):
     __tablename__ = "menu_item"
 
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    course = Column(String(80))
+    description = Column(String(250))
+    price = Column(String(8))
+    restaurant_id = Column(Integer, ForeignKey('restaurant_id'))
+    restaurant = relationship(Restaurant)
+
+
 engine = create_engine("sqlite:///restaurantmenu.db")
+Base.metadata.create_all(engine)
